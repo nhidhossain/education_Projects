46fb3f16b9c7467309e636f638d951dcd2d8e370
nhidhossain <120573052+nhidhossain@users.noreply.github.com>
2024-06-06 23:20:29 +0600

    Add files via upload

---
+++ education_projects/sum_of_two_numbers/sum_two_number.py
@@ -0,0 +1,11 @@
+#Get Input from User
+
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
+num3 = float(input("Enter the Third Number: "))
+
+#Sum of there three numbers
+sum = num1 + num2 + num3
+
+#Display the sum
+print ("The Sum of These Three number is: ", sum)
