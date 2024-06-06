24686502f835575621f7befc3a21655e691edcba
nhidhossain <120573052+nhidhossain@users.noreply.github.com>
2024-06-06 23:44:21 +0600

    Avg determination

---
+++ average_of_numbers/avg.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+sum = num1 + num2 + num3
+avg = sum / 3
+
+print("Average of These Numbers is: ", avg)
