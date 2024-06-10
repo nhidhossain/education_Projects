fc910f52eb918e2e0893c3d9beda24fffe406f39
nhidhossain <120573052+nhidhossain@users.noreply.github.com>
2024-06-10 12:35:44 +0600

    area of circle and rectangle

--- area-of_circle/area_of_circle.py
+++ area-of_circle/area_of_circle.py
@@ -1,1 +1,11 @@
-print ("Hello World")
+# formula area of rect is A=pi*r**2
+# here, pi = 3.1416 and r = radius 
+# the superscript of 2 is "cm\U+00B2"
+
+import math
+
+r = float(input("Enter the Radius of the Circle: "))
+
+area = math.pi*(r**2)
+
+print("The area of your rectangle is: {} cm\u00b2".format(area))
--- area_of_rectangle/area_of_rectangle.py
+++ area_of_rectangle/area_of_rectangle.py
@@ -1,1 +1,12 @@
-print ("Hello World")
+# formula area of rect is A=wl
+# here, w = width and l = length 
+#       or we can use A = bh
+#       here, b = base and h = height
+# the superscript of 2 is "\U+00B2"
+
+w = float(input("Enter the width of the Rectangle: "))
+l = float(input("Enter the length of the Rectangle: "))
+
+area = w * l 
+
+print("The area of your rectangle is: {} m\u00b2".format(area))
