5cb5e3117ab27a42d325037613534950f4c3cb19
nhidhossain <120573052+nhidhossain@users.noreply.github.com>
2024-06-13 18:36:10 +0600

    surface area of sphere

--- area_of_sphere/area_of_sphere.py
+++ area_of_sphere/area_of_sphere.py
@@ -1,1 +1,22 @@
-print ("Hello World")
+import math
+
+radius = float(input("Enter the Radius of the Sphere: "))
+
+Area = 4 * math.pi * radius**2 
+
+print(f"The Surface Area of the Sphere is: {Area:.2f}", "square units.")
+
+# -------
+
+import math
+
+def surface_area_of_sphere(radius):
+    
+    surface_area = 4 * math.pi * radius ** 2
+    return surface_area
+
+radius = float(input("Enter the radius of the sphere: "))
+
+area = surface_area_of_sphere(radius)
+
+print(f"The surface area of the sphere with radius {radius} is {area:.2f} square units.")
